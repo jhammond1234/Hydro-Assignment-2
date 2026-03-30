1c87d1ce3b813ba70432730b7bb64430869863f8
Jackson Hammond <u1405836@utah.edu>
2026-03-30 12:33:35 -0600

    processSNTL

---
+++ supporting_scripts/dataprocessing.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import os
+
+# def processSNOTEL(site, stateab, WYOI):
+#     print(site)
+
+#     sitedf = pd.read_csv(f"files/SNOTEL/df_{site}.csv")
+
+#     WYs = sitedf['Water_Year'].unique()
+
+#     WYsitedf = pd.DataFrame()
+
+#     for WY in WYs:
+#         cols =['M', 'D', 'Snow Water Equivalent (m) Start of Day Values']
+
+#         #get water year of interest
+#         wydf = sitedf[sitedf['Water_Year']==WY]
+#         wydf['M'] = pd.to_datetime(sitedf['Date']).dt.month
+#         wydf['D'] = pd.to_datetime(sitedf['Date']).dt.day
+
+#         #change NaN to 0, most NaN values are from low to 0 SWE measurements
+#         wydf['Snow Water Equivalent (m) Start of Day Values'] = wydf['Snow Water Equivalent (m) Start of Day Values'].fillna(0)
+#         wydf = wydf[cols]
+#         wydf.rename(columns = {'Snow Water Equivalent (m) Start of Day Values':f"{WY}_SWE_m"}, inplace=True)
+#         wydf.reset_index(inplace=True, drop=True)
+#         WYsitedf[f"{WY}_SWE_in"] = wydf[f"{WY}_SWE_m"]*39.3701 #converting m to inches (standard for snotel)
+
+#         if len(wydf) == 365:
+#             try:
+#                 WYsitedf.insert(0,'M',wydf['M'])
+#                 WYsitedf.insert(1,'D',wydf['D'])
+#             except:
+#                 pass
+#     #WYsitedf.fillna(0)
+
+#     #remove July, August, September
+#     months = [8,9]
+#     WYsitedf = WYsitedf[~WYsitedf['M'].isin(months)]
+
+#     #remove M/D to calculate row min, mean, median, max tiers
+#     df = WYsitedf.copy()
+#     #drop the water year of interest from WYsitedf to calculate the min, mean, median, max SWE for each day of the water year across all other years of data available for that site
+    
+#     print(f"Dropping {WYOI} from the calculations of the min, mean, median, max SWE for each day of the water year across all other years of data available for that site")
+#     try:
+#         WYOIdrop = f"{WYOI}_SWE_in"
+#         coldrop = ['M', 'D', WYOIdrop]
+#         WYsitedf = WYsitedf.drop(columns = coldrop)
+#     except:
+#         print(f"{WYOI} not found in the data, not dropping any columns")
+    
+    
+#     df['min'] = WYsitedf.min(axis=1)
+#     df['Q10'] = WYsitedf.quantile(0.10, axis=1)
+#     df['Q25'] = WYsitedf.quantile(0.25, axis=1)
+#     df['mean'] = WYsitedf.mean(axis=1)
+#     df['median'] = WYsitedf.median(axis=1)
+#     df['Q75'] = WYsitedf.quantile(0.75, axis=1)
+#     df['Q90'] = WYsitedf.quantile(0.90, axis=1)
+#     df['max'] = WYsitedf.max(axis=1)
+
+#     #add back in M/d for plotting
+#     # df.insert(0,'M',WYsitedf['M'])
+#     # df.insert(1,'D',WYsitedf['D'])
+
+#     # Convert to datetime format
+#     df['date'] = pd.to_datetime(dict(year = 2023, month = df['M'], day = df['D'])) 
+
+#     # Format the date
+#     df['M-D'] = df['date'].dt.strftime('%m-%d')
+#     df.set_index('M-D', inplace=True)
+
+#     return df
+
+def processSNOTEL(site, stateab):
+    print(site)
+
+    sitedf = pd.read_csv(f"files/SNOTEL/df_{site}.csv")
+
+     # Convert datetime and add Water_Year column
+    sitedf['datetime'] = pd.to_datetime(sitedf['datetime'])
+    sitedf['Water_Year'] = sitedf['datetime'].apply(lambda x: x.year + 1 if x.month >= 10 else x.year)
+    sitedf['Snow Water Equivalent (m) Start of Day Values'] = sitedf['WTEQ'] * 0.0254
+    sitedf['Date'] = sitedf['datetime']
+
+    WYs = sitedf['Water_Year'].unique()
+
+    WYs = sitedf['Water_Year'].unique()
+
+    WYsitedf = pd.DataFrame()
+
+    for WY in WYs:
+        cols =['M', 'D', 'Snow Water Equivalent (m) Start of Day Values']
+
+        wydf = sitedf[sitedf['Water_Year']==WY]
+        wydf['M'] = pd.to_datetime(sitedf['Date']).dt.month
+        wydf['D'] = pd.to_datetime(sitedf['Date']).dt.day
+
+        wydf['Snow Water Equivalent (m) Start of Day Values'] = wydf['Snow Water Equivalent (m) Start of Day Values'].fillna(0)
+        wydf = wydf[cols]
+        wydf.rename(columns = {'Snow Water Equivalent (m) Start of Day Values':f"{WY}_SWE_m"}, inplace=True)
+        wydf.reset_index(inplace=True, drop=True)
+        WYsitedf[f"{WY}_SWE_in"] = wydf[f"{WY}_SWE_m"]*39.3701
+
+        if len(wydf) == 365:
+            try:
+                WYsitedf.insert(0,'M',wydf['M'])
+                WYsitedf.insert(1,'D',wydf['D'])
+            except:
+                pass
+
+    months = [8,9]
+    WYsitedf = WYsitedf[~WYsitedf['M'].isin(months)]
+
+    df = WYsitedf.copy()
+    
+    df['min'] = WYsitedf.min(axis=1)
+    df['Q10'] = WYsitedf.quantile(0.10, axis=1)
+    df['Q25'] = WYsitedf.quantile(0.25, axis=1)
+    df['mean'] = WYsitedf.mean(axis=1)
+    df['median'] = WYsitedf.median(axis=1)
+    df['Q75'] = WYsitedf.quantile(0.75, axis=1)
+    df['Q90'] = WYsitedf.quantile(0.90, axis=1)
+    df['max'] = WYsitedf.max(axis=1)
+
+    df['date'] = pd.to_datetime(dict(year = 2023, month = df['M'], day = df['D'])) 
+    df['M-D'] = df['date'].dt.strftime('%m-%d')
+    df.set_index('M-D', inplace=True)
+
+
+    # Save processed data
+    OutputFolder = 'files/SNOTEL_processed'
+    if not os.path.exists(OutputFolder):
+        os.makedirs(OutputFolder)
+    df.to_csv(f'{OutputFolder}/{site}_processed.csv')
+
+    return df
